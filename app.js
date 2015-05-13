2bbbde027d7ae8f91403afb9917e9c70cfba0fbb
umarfarooq360 <omar.farooq@richmond.edu>
2015-05-13 14:50:15 -0400

    added book autocomplete

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var db = monk('mongodb://admin:passw0rd@proximus.modulusmongo.net:27017/ohemU9du
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var autocomplete = require('./routes/autocomplete');
 
 var app = express();
 
@@ -43,16 +42,6 @@ app.use('/users', users);
 
 //-----------code for autocomplete----
 
-app.configure(function () {
- app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
- app.use(express.bodyParser());
-});
-
-
-app.get('/autocomplete/:search',autocomplete.find);
-
-app.listen(6000);
-console.log('Listening on port 3000...');
 //------------------------------
 
 
---
+++ public/autocomplete.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {  // only begin once page has loaded
+    var $searchBox = $('#inputName'); //will check for jQ
+
+    $searchBox.autocomplete({ // attach auto-complete functionality to textbox
+        // define source of the data
+        source: function (request, response) {
+            // url link to google books, including text entered by user (request.term)
+            var booksUrl = "https://www.googleapis.com/books/v1/volumes?printType=books&q=" + encodeURIComponent(request.term);
+            $.ajax({
+                url: booksUrl,
+                dataType: "jsonp",
+                success: function(data) {
+                    response($.map(data.items, function (item) {
+                        if (item.volumeInfo.authors && item.volumeInfo.title && item.volumeInfo.industryIdentifiers && item.volumeInfo.publishedDate)
+                        {
+                            return {
+                                // label value will be shown in the suggestions
+                                label: item.volumeInfo.title + ', ' + item.volumeInfo.authors[0] + ', ' + item.volumeInfo.publishedDate,
+                                // value is what gets put in the textbox once an item selected
+                                value: item.volumeInfo.title,
+                                // other individual values to use later
+                                title: item.volumeInfo.title,
+                                author: item.volumeInfo.authors[0],
+                                isbn: item.volumeInfo.industryIdentifiers,
+                                publishedDate: item.volumeInfo.publishedDate,
+                                image: (item.volumeInfo.imageLinks == null ? "" : item.volumeInfo.imageLinks.thumbnail),
+                                description: item.volumeInfo.description,
+                            };
+                        }
+                    }));
+                }
+            });
+        },
+        select: function (event, ui) {
+            // what to do when an item is selected
+            // first clear anything that may already be in the description
+            $('#divDescription').empty();
+            // we get the currently selected item using ui.item
+            // show a pic if we have one
+            if (ui.item.image != '')
+            {
+                $('#divDescription').append('<img src="' + ui.item.image + '" style="float: left; padding: 10px;">');
+            }
+            // and title, author, and year
+            $('#divDescription').append('<p><b>Title:</b> ' + ui.item.title  + '</p>');
+            $('#divDescription').append('<p><b>Author:</b> ' + ui.item.author  + '</p>');
+            $('#divDescription').append('<p><b>First published year:</b> ' + ui.item.publishedDate  + '</p>');          
+            // and the usual description of the book
+            $('#divDescription').append('<p><b>Description:</b> ' + ui.item.description  + '</p>');
+            // and show the link to oclc (if we have an isbn number)
+            if (ui.item.isbn && ui.item.isbn[0].identifier)
+            {
+                $('#divDescription').append('<P><b>ISBN:</b> ' + ui.item.isbn[0].identifier + '</p>');
+                $('#divDescription').append('<a href="http://www.worldcat.org/isbn/' + ui.item.isbn[0].identifier + '" target="_blank">View item on worldcat</a>');
+            }
+        },
+        minLength: 2 // set minimum length of text the user must enter
+    });
+
+  });
