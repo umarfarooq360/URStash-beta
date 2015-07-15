e658f9a59e8d8e987e1c9b6bfae8caf29fb29d68
umarfarooq360 <omar.farooq@richmond.edu>
2015-07-15 19:48:35 -0400

    removed autoscrolling and made index page responsive design

--- public/js/onepage.init.js
+++ public/js/onepage.init.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
     	sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE'],
-    	scrollBar: true
+    	scrollBar: true,
+    	autoScrolling:false
 	});
 });
