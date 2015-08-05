e7946638a9f0c328c4fd4e83ade0f0e37a708072
Shiv Toolsidass <shiv.toolsidass@richmond.edu>
2015-08-05 18:25:57 +0530

    05augchanges
    Latest changes to Login/SignUp page - field validation for password,
    email and number on Signup page

--- public/js/validate.js
+++ public/js/validate.js
@@ -1,92 +1,122 @@
  
-var $username = $("#emailForm");
-var $password = $("#passForm");
-var $usernameWarning = $("#emailError");
-var $passwordWarning = $("#passwordError");
+// var $username = $("#emailForm");
+// var $password = $("#passForm");
+// var $usernameWarning = $("#emailError");
+// var $passwordWarning = $("#passwordError");
 
-$usernameWarning.hide();
-$passwordWarning.hide();
+// $usernameWarning.hide();
+// $passwordWarning.hide();
 
-function isUsernameValid(){
-	return $username.val().length > 4;
-}
+// function isUsernameValid(){
+// 	return $username.val().length > 4;
+// }
 
-function isPasswordValid(){
-	return $password.val().length > 6;
-}
+// function isPasswordValid(){
+// 	return $password.val().length > 6;
+// }
 
-function userNameEvent(){
-	if(isUsernameValid()){
-		$usernameWarning.hide();
-	}
-	else{
-		$usernameWarning.show();
-	}
-}
+// function userNameEvent(){
+// 	if(isUsernameValid()){
+// 		$usernameWarning.hide();
+// 	}
+// 	else{
+// 		$usernameWarning.show();
+// 	}
+// }
 
-function passwordEvent(){
-	if(isPasswordValid()){
-		return $passwordWarning.hide();
-	}
-	else{
-		return $passwordWarning.show();
-	}
+// function passwordEvent(){
+// 	if(isPasswordValid()){
+// 		return $passwordWarning.hide();
+// 	}
+// 	else{
+// 		return $passwordWarning.show();
+// 	}
 
-}
+// }
 
-function canSubmit(){
-	return isUsernameValid() && isPasswordValid();
-}
+// function canSubmit(){
+// 	return isUsernameValid() && isPasswordValid();
+// }
 
-function enableSubmit(){
-	$("btnlogin").prop("disabled", !canSubmit());
-}
+// function enableSubmit(){
+// 	$("btnlogin").prop("disabled", !canSubmit());
+// }
 
-$password.focus(passwordEvent).keyup(passwordEvent).keyup(userNameEvent).keyup(enableSubmit);
-$username.focus(userNameEvent).keyup(userNameEvent).keyup(passwordEvent).keyup(enableSubmit);
+// $password.focus(passwordEvent).keyup(passwordEvent).keyup(userNameEvent).keyup(enableSubmit);
+// $username.focus(userNameEvent).keyup(userNameEvent).keyup(passwordEvent).keyup(enableSubmit);
 
-var $signUp = $("#signup");
-var $login = $("#login")
-var $signupbutton = $("#signupbutton");
-var $loginbutton = $("#loginbutton");
-var $emailPrompt = $("#emailPrompt");
-var $passwordPrompt = $("#passwordPrompt");
-var $numberPrompt = $("#numberPrompt");
-var $emailForm = $("#emailForm");
-var $passwordForm = $("#passwordForm");
-var $numberForm = $("#lastSignUp");
+// var $signUp = $("#signup");
+// var $login = $("#login")
+// var $signupbutton = $("#signupbutton");
+// var $loginbutton = $("#loginbutton");
+// var $emailPrompt = $("#emailPrompt");
+// var $passwordPrompt = $("#passwordPrompt");
+// var $numberPrompt = $("#numberPrompt");
+// var $emailForm = $("#emailForm");
+// var $passwordForm = $("#passwordForm");
+// var $numberForm = $("#lastSignUp");
 
 //Dont hide this anymore
 //$signUp.hide();
 
-$emailForm.focus(function(){
-	$emailPrompt.show();
-	$emailPrompt.siblings("div").hide();
+$("#validEmail").hide();
+$("#validPassword").hide();
+$("#validNumber").hide();
+
+$("#emailForm2").focus(function(){
+	$("#validPassword").hide();
+	$("#validNumber").hide();
+	$("#validEmail").fadeIn("slow");
+}).keyup(function(){
+	$(this).css("border-color", "red");
+	var val = $(this).val();
+	if(val.includes('richmond.edu')){
+		$(this).css("border-color", "green");
+	}
 });
 
-$numberForm.focus(function(){
-	$numberPrompt.show();
-	$numberPrompt.siblings("div").hide();
+$("#passwordForm").focus(function(){
+	$("#validEmail").hide();
+	$("#validNumber").hide();
+	$("#validPassword").fadeIn("slow");
+}).keyup(function(){
+	$(this).css("border-color", "red");
+	var val = $(this).val();
+	if(val.length >= 6){
+		$(this).css("border-color", "green");
+	}
 });
 
-$passwordForm.focus(function(){
-	$passwordPrompt.show();
-	$passwordPrompt.siblings("div").hide();
+
+
+$("#lastSignUp").focus(function(){
+	$("#validEmail").hide();
+	$("#validPassword").hide();
+	$("#validNumber").fadeIn("slow");
+}).keyup(function(){
+	$(this).css("border-color", "red");
+	var val = $(this).val();
+	var phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+if (phoneRegex.test(val)) {
+   $(this).css("border-color", "green");
+} else {
+    // Invalid phone number
+}
 });
 
-//things to correct here: 1) Slidetoggle makes forms disappear when clicked again
-						//2) Animation effects, implement some cool shit here with animate function
 
-//Changed to use slides of fullPage scroll 
-$signupbutton.on('click', function(){
-	$.fn.fullpage.moveSlideRight();
-});
 
-$loginbutton.on('click', function(){
-	$.fn.fullpage.moveSlideRight();
-});
+// $numberForm.focus(function(){
+// 	$numberPrompt.show();
+// 	$numberPrompt.siblings("div").hide();
+// });
 
-enableSubmit();
+// $passwordForm.focus(function(){
+// 	$passwordPrompt.show();
+// 	$passwordPrompt.siblings("div").hide();
+// });
+
+
 
 
 
